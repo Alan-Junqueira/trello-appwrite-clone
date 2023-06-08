38122acbf96d08e7aaa933be0a9560fc8d146d1a
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-06-08 18:29:41 -0300

    feat: build the Column component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,6 +3,7 @@
 import { useBoardStore } from '@/store/BoardStore';
 import { useEffect } from 'react';
 import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
+import { Column } from './Column';
 
 export const Board = () => {
   const {
---
+++ src/components/Column.tsx
@@ -0,0 +1,83 @@
+import { PlusCircleIcon } from '@heroicons/react/24/solid'
+import React from 'react'
+import { Draggable, Droppable } from 'react-beautiful-dnd'
+
+interface IColumn {
+  id: TypedColumn
+  todos: Todo[]
+  index: number
+}
+
+const idToColumnText: { [key in TypedColumn]: string } =
+{
+  todo: "To Do",
+  inprogress: "In Progress",
+  done: "Done"
+}
+
+export const Column = ({ id, index, todos }: IColumn) => {
+  return (
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <Droppable
+            droppableId={index.toString()}
+            type='card'
+          >
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`
+                  p-2 rounded-2xl shadow-sm
+                  ${snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"}
+                `}
+              >
+                <h2 className='flex justify-between font-bold text-xl p-2'>
+                  {idToColumnText[id]}
+                  <span className='text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal'>
+                    {todos.length}
+                  </span>
+                </h2>
+
+                <div className="space-y-2">
+                  {todos.map((todo, index) => (
+                    <Draggable
+                      key={todo.$id}
+                      draggableId={todo.$id}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <TodoCard
+                          todo={todo}
+                          index={index}
+                          id={id}
+                          innerRef={provided.innerRef}
+                          draggableProps={provided.draggableProps}
+                          dragHandleProps={provided.dragHandleProps}
+                        />
+                      )}
+                    </Draggable>
+                  ))}
+
+                  {/* Create a space when a item is draggable */}
+                  {provided.placeholder}
+
+                  <div>
+                    <button className='flex items-end justify-end p-2'>
+                      <PlusCircleIcon className='h-10 w-10' />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
+  )
+}
